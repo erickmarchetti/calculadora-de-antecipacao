c0df2998975cfc0d6e9ae267e2a6b824415cf358
ErickMarchetti <erickgmarchetti@gmail.com>
2022-11-14 14:14:14 -0300

    feat: estrutura html inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
 function App() {
-  return <></>
+  return (
+    <div>
+      <div>
+        <h1>h1</h1>
+        <form>
+          <div>
+            <span></span>
+            <input />
+            <span></span>
+          </div>
+          <div>
+            <span></span>
+            <input />
+            <span></span>
+          </div>
+          <div>
+            <span></span>
+            <input />
+            <span></span>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h2>h2</h2>
+        <div></div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App
